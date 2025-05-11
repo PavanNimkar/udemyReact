c427f089106aaf73a32157594ba095f3e6df8201
PavanNimkar <pavannnimkar@gmail.com>
2025-05-11 06:46:10 +0530

    initial commits

---
+++ stylingInReact/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+const main = {
+  background: "red",
+  height: "100vh",
+  width: "100%",
+};
+
+function App() {
+  return (
+    <>
+      <div style={main}></div>
+    </>
+  );
+}
+
+export default App;
