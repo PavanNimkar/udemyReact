910f30a387b70e734912670a51af068d87049548
PavanNimkar <pavannnimkar@gmail.com>
2025-05-11 06:57:06 +0530

    added template literals

---
+++ templateLiterals/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+const firstVar = "Expert";
+const secondVar = "Developer";
+
+function App() {
+  return (
+    <>
+      <h1>{`We are ${firstVar} ${secondVar}`}</h1>
+    </>
+  );
+}
+
+export default App;
